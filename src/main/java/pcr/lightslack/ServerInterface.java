b966cb13e94371a9cb02ae4614abea6be515e0a7
randriamalala <randriamalal@gmail.com>
2020-04-17 09:33:58 +0200

    [Server] Adds rmi RemoteException

--- src/main/java/pcr/lightslack/ServerInterface.java
+++ src/main/java/pcr/lightslack/ServerInterface.java
@@ -1,9 +1,10 @@
 package pcr.lightslack;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
 public interface ServerInterface extends Remote {
-    List<String> returnMessages();
-    List<String> addMessage(String m);
+    List<String> returnMessages() throws RemoteException;
+    List<String> addMessage(String m) throws RemoteException;
 }
